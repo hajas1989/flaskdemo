3db7e19a3a66782e1a21015890a97ed2392219ee
hajashashim <37581388+hajashashim@users.noreply.github.com>
2018-06-20 14:52:48 +0530

    response message updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask service executed successfully !!!"
+    return "Flask service demo."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
